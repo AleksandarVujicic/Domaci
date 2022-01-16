d87b66bfcd8d9f53b7274c396a7ed89013c738a3
Aleksandar Vujicic <aleksaandar9933@gmail.com>
2022-01-16 09:05:24 +0100

    dva printa

--- src/p13_01_2022/Zadatak1.java
+++ src/p13_01_2022/Zadatak1.java
@@ -5,10 +5,13 @@ public class Zadatak1 {
 	public static void main(String[] args) {
 		
 		FacebookPost post1 = new FacebookPost("Aleksandar Vujicic", "Kristina Vujicic", "Hello World");
+		FacebookPost post2 = new FacebookPost("Nikola Nikolic", "Pera Peric", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ut aliquam purus sit amet luctus venenatis.");
 		
-		
+		post1.Share();
 		post1.Print();
-		
+		System.out.println("");
+		post2.Lajk();
+		post2.Print();
 		
 
 	}
